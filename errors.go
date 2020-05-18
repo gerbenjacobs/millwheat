873dea36a58d39ec1c03b2be5934df50e90779d1
Gerben Jacobs <gerben@kramphub.com>
2020-05-18 16:35:41 +0200

    bump chota and add images

--- errors.go
+++ errors.go
@@ -8,8 +8,6 @@ var (
 	ErrUserNotFound        = errors.New("user not found")
 	ErrUserEmailUniqueness = errors.New("email address already in use")
 
-	ErrCharacterNotFound = errors.New("character not found")
-
 	ErrEmailNotFound = errors.New("no user found for email")
 	ErrWrongPassword = errors.New("password doesn't match")
 
